c146a8a844308b09ee72e87045d8b271def5882b
Bernie Seabrook <bseabrook@hotmail.com>
2024-08-10 14:04:01 -0700

    better

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
     <main>
         <div v-if="mounted">
-            <div>{{ app.completedSteps }} of {{ app.totalSteps }} completed</div>
-            <div v-for="step in app.steps">
-                <div>
-                    <span style="font-size:x-large">{{ step.name }}</span>
-                    <span style="color:red"> {{ step.status }}</span>
-                    <div v-for="subStep in step.subSteps">
-                        <span style="font-size:medium">{{ subStep.name }}</span>
-                        <span style="color:blue"> {{ subStep.status }}</span>
-                    </div>
+            <ol v-for="step in app.steps">
+                <li>
+                    {{ step.text }} {{ step.status }}
 
-                </div>
-            </div>
-            <div>
-                <span @click="back"> [Back] </span>
-                <span @click="next"> [Next] </span>
-            </div>
+                    <ul v-for="substep in app.getSubSteps(step.id)">
+                        <li> {{ substep.text }}  {{ substep.status }} </li>
+                    </ul>
+                </li>
+            </ol>
+            <div>You are currently working on: {{ app.currentSubStep }}</div>
         </div>
     </main>
 </template>
--- src/next-level-ira-application.js
+++ src/next-level-ira-application.js
@@ -34,94 +34,51 @@ class ApplicationData {
     }
 }
 
-class SubStep {
-    constructor(name, status = Status.NotStarted) {
-        this.name = name;
-        this.status = status;
-        this.hidden = false;
-    }
-}
-
-class Step {
-    constructor(name, status = Status.NotStarted) {
-        this.name = name;
-        this.status = status;
-
-        this._subSteps = [];
-
-        console.log("Step being created:", name);
-
-        if (name == "Identification information") {
-            this._subSteps = [
-                new SubStep("Acknowledgements"),
-                new SubStep("Personal details"),
-                new SubStep("Legal residency status"),
-                new SubStep("Social Security Number"),
-                new SubStep("Address"),
-                new SubStep("Photo ID"),
-                new SubStep("Contact information"),
-            ];
-
-            this._subSteps[3].hidden = true;
-        }
-
-        else if (name == "Employment information") {
-            this._subSteps = [
-                new SubStep("Employer information"),
-                new SubStep("Employment information details"),
-                new SubStep("Income")
-            ];
-        }
-        else if (name == "Terms and conditions") {
-            this._subSteps = [
-                new SubStep("Agreement 1"),
-                new SubStep("Agreement 2")
-            ];
-        }
-        else if (name == "Review and submit") {
-            this._subSteps = [
-                new SubStep("Review and submit your NextLevel IRA application")
-            ];
-        }
-    }
-
-    get subSteps() {
-        console.log("Step", this.name, "has", this._subSteps.length, "sub-steps.");
-        return this._subSteps;
-    }
-
-}
-
-
 class NextLevelIraApplication {
     constructor() {
         this.data = new ApplicationData();
-
-        this._steps = [
-            new Step("Identification information"),
-            new Step("Employment information"),
-            new Step("Terms and conditions"),
-            new Step("Review and submit")
+        this.rows = [
+            { id: 1, parentId: null, type: "step", text: "Identification information", status: Status.NotStarted },
+            { id: 2, parentId: 1, type: "substep", text: "Acknowledgements", status: Status.NotStarted },
+            { id: 3, parentId: 1, type: "substep", text: "Personal details", status: Status.NotStarted },
+            { id: 4, parentId: 1, type: "substep", text: "Legacy residency status", status: Status.NotStarted },
+            { id: 5, parentId: 1, type: "substep", text: "Social security number", status: Status.NotStarted },
+            { id: 6, parentId: 1, type: "substep", text: "Address", status: Status.NotStarted },
+            { id: 7, parentId: 1, type: "substep", text: "Photo Id", status: Status.NotStarted },
+            { id: 8, parentId: 1, type: "substep", text: "Contact information", status: Status.NotStarted },
+            { id: 9, parentId: null, type: "step", text: "Employment information", status: Status.CannotStart },
+            { id: 10, parentId: 9, type: "substep", text: "Employer information", status: Status.NotStarted },
+            { id: 11, parentId: 9, type: "substep", text: "Employment information", status: Status.NotStarted },
+            { id: 12, parentId: 9, type: "substep", text: "Income", status: Status.NotStarted },
+            { id: 13, parentId: null, type: "step", text: "Terms and conditions", status: Status.CannotStart },
+            { id: 14, parentId: 13, type: "substep", text: "Agreement 1", status: Status.NotStarted },
+            { id: 15, parentId: 13, type: "substep", text: "Agreement 2", status: Status.NotStarted },
+            { id: 16, parentId: null, type: "step", text: "Review and submit", status: Status.CannotStart },
+            { id: 17, parentId: 16, type: "substep", text: "Review and submit your NextLevel IRA application", status: Status.NotStarted },
         ];
+
+        this.setCurrentSubStep(2);
     }
 
     get steps() {
-        return this._steps;
+        return this.rows.filter(x => x.type == 'step');
     }
 
-    get completedSteps() {
-        return this._steps.filter(x => x.status == Status.Completed).length;
+    getSubSteps(parentId) {
+        return this.rows.filter(x => x.parentId == parentId);
     }
 
-    get totalSteps() {
-        return this._steps.length;
-    }
-    get currentStep() {
-        return this.steps.find(x => x.status == Status.InProgress);
+    setCurrentSubStep(id) {
+        var rows = this.rows.filter(x => x.id == id);
+        if (rows.length > 0) {
+            var currentSubStep = rows[0];
+            currentSubStep.status = Status.InProgress;
+            this._currentSubStep = currentSubStep;
+        }
     }
 
-    moveToNextStep() {
-        console.log("moving to next step");
+    get currentSubStep() {
+        return this._currentSubStep;
     }
 }
 
