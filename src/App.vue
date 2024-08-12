df5c5d7261bafc1a0f89cf24372f00f83eb1b0da
Bernie <bseabrook@penchecks.com>
2024-08-12 11:50:52 -0700

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,205 +1,66 @@
 <template>
     <main>
-        <div v-if="mounted">
-            <div>You have completed {{ stepWizard.completedStepCount }} of {{ stepWizard.totalStepCount }} steps</div>
-            <div v-if="view == 'instructions'">
-                <h1>Instruction Mode</h1>
+        <div v-if="application">
+            <div>Your application is: {{ application.status }}</div>
+            <div>
                 <ol>
-                    <li v-for="group in groups">
-                        {{ group }} {{ stepWizard.getGroupStatus(group) }}
-                        <ul v-for="step in stepWizard.getGroupSteps(group)">
-                            <li @click="stepClicked(step)"> {{ step.text }}  {{ step.status }} </li>
+                    <li v-for="step in application.steps">
+                        {{ step.name }} ({{ step.status }})
+                        <ul v-for="step in step.steps">
+                            <li>
+                                {{ step.name }} ({{ step.status }})
+                                <span v-if="step.canStartStep">
+                                    <span style="text-decoration: underline;" @click="editStep(step)">edit</span>
+                                </span>
+                            </li>
                         </ul>
                     </li>
                 </ol>
             </div>
-
-            <div v-else-if="view == 'summary'">
-                <h1>Summary Mode</h1>
-                <ol>
-                    <li v-for="group in groups">
-                        {{ group }}
-                        <ul v-for="step in stepWizard.getGroupSteps(group)">
-                            <li @click="stepClicked(step)"> {{ step.text }}  {{ step.status }} </li>
-                        </ul>
-                    </li>
-                </ol>
-            </div>
-
-            <div v-else-if="view == 'edit'">
-                <h1>Edit Mode</h1>
-
-                <input type="button" value="Back" @click="goBack()" />
-
-                <div v-if="stepWizard.currentStep.id == 0">
-                    <h1>Acknowledgements</h1>
-                    <table>
-                        <tr>Has Acknowledgement 1<td></td><td>{{ applicationData.isAcknowledged }}</td></tr>
-                        <tr>Is Foreign Actor<td></td><td>{{ applicationData.isForeignActor }}</td></tr>
-                    </table>
-                </div>
-
-                <div v-else-if="stepWizard.currentStep.Id == 1">
-                    <h1>Personal details</h1>
-                    <table>
-                        <tr>
-                            <td>First Name: </td>
-                            <td>{{ applicationData.firstName }}</td>
-                        </tr>
-                        <tr>
-                            <td>Last Name: </td>
-                            <td>{{ applicationData.lastName }}</td>
-                        </tr>
-                    </table>
-                </div>
-
-                <div v-else>
-                    Step {{ stepWizard.currentStep.id }} not implemented
-                </div>
-                <input type="button" value="Save and continue" @click="saveAndContinue()" />
+            <div v-if="viewMode == 'edit'">
+                <div>You are editing step {{ currentStep.name }} </div>
+                <span style="text-decoration: underline;" @click="markStepCompleted(currentStep)">valid</span> &nbsp;
+                <span style="text-decoration: underline;" @click="markStepInProgress(currentStep)">invalid</span>
             </div>
         </div>
+
     </main>
 </template>
 
 <script>
-    import { Status, Step, StepWizard } from "@/step-wizard.js";
+    import { Application, Status, Step } from "@/step-wizard.js";
 
     export default {
 
         data: function () {
             return {
-                applicationData: null,
-                stepWizard: null,
-                view: null,
-                mounted: false,
+                currentStep: null,
+                viewMode: 'instructions',
+                application: null,
             };
         },
 
-        mounted: function () {
-            this.applicationData = this.getApplicationData();
-            this.stepWizard = new StepWizard(this.getSteps());
-            this.setView("instructions");
-            this.mounted = true;
-        },
-
-        computed: {
-            groups: function () {
-                return this.stepWizard.getGroups();
-            }
-        },
-
         methods: {
-            getApplicationData() {
-                return {
-                    isAcknowledged: false,
-                    isForeignActor: false,
-                    firstName: "Bernie",
-                    lastName: "Seabrook",
-                    dateOfBirth: null,
-                    legalResidencyStatus: null,
-                    taxId: null,
-                    address: null,
-                    address2: null,
-                    city: null,
-                    state: null,
-                    postalCode: null,
-                    country: null,
-                    otherRegion: null,
-                    isMailingAddressSame: false,
-                    photoIdType: '',
-                    phoneNumber: null,
-                    isCannabisRelatedBusiness: null,
-                    occupation: null,
-                    isEmployed: null,
-                    expectedAnnualIncome: 0,
-                    expectedRolloverAmount: 0,
-                };
+            editStep(step) {
+                this.currentStep = step;
+                this.viewMode = 'edit';
             },
 
-            getSteps() {
-                var steps = [
-                    new Step("Identification information", "Acknowledgements"),
-                    new Step("Identification information", "Personal details"),
-                    new Step("Identification information", "Legacy residency status"),
-                    new Step("Identification information", "Social security number"),
-                    new Step("Identification information", "Address"),
-                    new Step("Identification information", "Photo Id"),
-                    new Step("Identification information", "Contact information"),
-                    new Step("Employment information", "Employer information"),
-                    new Step("Employment information", "Employment information"),
-                    new Step("Employment information", "Income"),
-                    new Step("Terms and conditions", "Agreement 1"),
-                    new Step("Terms and conditions", "Agreement 2"),
-                    new Step("Review and submit", "Review and submit your NextLevel IRA application"),
-                ];
-
-                return steps;
+            markStepCompleted(step) {
+                this.application.markStepCompleted(step);
+                this.viewMode = 'instructions';
             },
 
-            setView(view) {
-                console.log('changing view to', view);
-                this.view = view;
+            markStepInProgress(step) {
+                this.application.markStepInProgress(step);
+                this.viewMode = 'instructions';
             },
+        },
 
-            stepClicked(step) {
-                console.log('step', step.id, 'clicked');
-
-                switch (this.view) {
-                    case 'instructions':
-                        if (!step.canBeStarted) {
-                            console.log('Step cannot be started', step);
-                            return;
-                        }
-                        console.log('Step started', step);
-                        step.status = Status.InProgress;
-                        this.stepWizard.startStep(step);
-                        this.setView('edit');
-                        break;
-                    case 'summary':
-                        this.stepWizard.resumeStep(step);
-                        this.setView('edit');
-                        break;
-                }
-            },
-
-            goBack() {
-                this.setView('instructions');
-            },
-
-            loadFromJson(json) {
-                var obj = JSON.parse(json);
-                this.view = obj.view;
-                this.stepWizard = obj.stepWizard;
-            },
-
-            save() {
-                var obj = [
-                    this.view,
-                    this.stepWizard
-                ];
-
-                var json = JSON.stringify(obj);
-
-                console.log(json);
-            },
-
-            saveAndContinue() {
-                var step = this.stepWizard.currentStep;
-
-                this.stepWizard.completeStep(step);
-
-                this.save();
-
-                if (this.stepWizard.allStepsCompleted) {
-                    this.setView('summary');
-                } else {
-                    this.setView('instructions');
-                }
-            },
-        }
+        mounted: function () {
+            this.application = new Application();
+        },
     }
-
 
 </script>
 
--- src/step-wizard.js
+++ src/step-wizard.js
@@ -4,90 +4,90 @@ const Status = {
     CannotStart: "cannotStart",
     NotStarted: "notStarted",
     InProgress: "inProgress",
-    Completed: "completed"
-}
+    Completed: "completed",
+};
 
 class Step {
-    static _id = 0;
-
-    constructor(group, text) {
-        this.group = group;
-        this.text = text;
+    constructor(name) {
+        this.parent = null;
+        this.name = name;
         this.status = Status.NotStarted;
-        this.id = Step._id;
-        Step._id++;
+        this.steps = [];
     }
 
-    get canBeStarted() {
-        if (this.status == Status.CannotStart) return false;
-        return true;
+    get isCompleted() {
+        return this.status == Status.Completed;
     }
 
-    get canBeCompleted() {
-        return true;
-    }
-}
-
-class StepWizard {
-    constructor(stepDetails) {
-        this.steps = stepDetails;
-        this.currentStep = null;
+    get canStartStep() {
+        return this.status != Status.CannotStart;
     }
 
-    get completedStepCount() {
-        return this.steps.filter(x=>x.status == Status.Completed).length;
-    }
-
-    get totalStepCount() {
-        return this.steps.length;
-    }
-
-    getGroups() {
-        const results = this.steps.map(x => x.group);
-        const uniqueArray = [...new Set(results)];
-        return uniqueArray;
-    }
-
-    getGroupSteps(group) {
-        return this.steps.filter(x => x.group == group);
-    }
-
-    getGroupStatus(group) {
-        var steps = this.getGroupSteps(group)
-        
-        // if the group cannot be started the steps couldn't be in progress or complete
-        if (group.status == Status.CannotStart) return Status.CannotStart;
-
-        // if all steps are complete then group is complete
-        if (steps.every(x => x.status == Status.Completed)) return Status.Completed; 
-
-        // if any steps are in progress or complete, then group is in progress
-        if (steps.some(x => x.status == Status.InProgress || x.status == Status.Completed)) return Status.InProgress; 
-
-        return Status.NotStarted;
-    }
-    get allStepsCompleted() {
-        return this.steps.filter(x=>x.status == Status.Completed).length == this.steps.length;
-    }
-
-    completeStep(step) {
-        if (step.status != Status.InProgress) throw new Error('Cannot complete step', step);
-        step.status = Status.Completed;
-        console.log('Step completed', step);
-    }
-
-    startStep(step) {
-        if (step.status == Status.CannotStart) throw new Error('Cannot start step', step);
-        this.currentStep = step;
-        console.log('Step started', step);
-    }
-
-    resumeStep(step) {
-        // if (step.status != Status.InProgress) throw new Error('Cannot resume step', step);
-        step.status = Status.InProgress;
-        this.currentStep = step;
-        console.log('Step resumed', step);
+    addStep(step) {
+        this.steps.push(step);
+        step.parent = this;
     }
 }
 
-export { Status, Step, StepWizard }
+class Application extends Step {
+    constructor() {
+        super("Application");
+        let step1 = new Step("Identification information")
+        step1.addStep(new Step("Acknowledgements"));
+        step1.addStep(new Step("Personal Details"));
+        step1.addStep(new Step("Legal residency status"));
+        step1.addStep(new Step("Address"));
+        step1.addStep(new Step("Photo ID"));
+        step1.addStep(new Step("Contact information"));
+
+        let step2 = new Step("Employment information");
+        step2.addStep(new Step("Employer information"));
+        step2.addStep(new Step("Personal Employment information"));
+        step2.addStep(new Step("Income"));
+
+        let step3 = new Step("Terms and conditions");
+        step3.addStep(new Step("Agreement 1"));
+        step3.addStep(new Step("Agreement 2"));
+
+        let step4 = new Step("Review and submit");
+        step4.addStep(new Step("review and submit your application"));
+
+        this.addStep(step1);
+        this.addStep(step2);
+        this.addStep(step3);
+    }
+
+    markStepNotStarted(step) {
+        this.setStepStatus(step, Status.NotStarted);
+    }
+
+    markStepCannotStart(step) {
+        this.setStepStatus(step, Status.CannotStart);
+    }
+
+    markStepCompleted(step) {
+        this.setStepStatus(step, Status.Completed);
+    }
+
+    markStepInProgress(step) {
+        this.setStepStatus(step, Status.InProgress);
+    }
+
+    setStepStatus(step, status) {
+        step.status = status;
+        for (var child of step.steps) child.status = status;
+        if (step.parent) this.reviewStepStatus(step.parent);
+    }
+
+    reviewStepStatus(step) {
+        if (step.steps.every(child => child.status == Status.Completed)) {
+            step.status = Status.Completed;
+        } else if (step.steps.some(child => child.status == Status.Completed || child.status == Status.InProgress)) {
+            step.status = Status.InProgress;
+        }
+
+        if (step.parent) this.reviewStepStatus(step.parent);
+    }
+}
+
+export { Application, Status, Step }
